ce5216fb31312eac87a522be16ce3d57b6a20f9b
Juste Zilinskaite <juste.zilinskaite@gmail.com>
2022-04-05 15:44:56 +0300

    Add basket service

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,43 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public BasketRepository(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _distributedCache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _distributedCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            var value = JsonConvert.SerializeObject(basket);
+
+            await _distributedCache.SetStringAsync(basket.UserName, value);
+
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
