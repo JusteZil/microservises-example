6066262b6e28e5295a8a6493691bc70e3f67e970
DEVB\juste.zilinskaite <juste.zilinskaite@devbridge.com>
2022-04-05 12:06:59 +0300

    Add catalog microservice

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # microservises-example
-Microservices-example.
+Based on Udemy course "Microservices Architecture and Implementation on .NET 5"
+https://github.com/aspnetrun/run-aspnetcore-microservices
+
+## Start development environment
+1. Navigate to the root directory which include **docker-compose.yml** files.
+2. Run following docker-compose command:
+```csharp
+docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
+```
+
+## Access microservises:
+* **Catalog API -> http://host.docker.internal:8000/swagger/index.html**
+
---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,85 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly ILogger<CatalogController> _logger;
+        private readonly IProductRepository _productRepository;
+
+        public CatalogController(ILogger<CatalogController> logger, IProductRepository productRepository)
+        {
+            _logger = logger;
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _productRepository.GetProduct(id);
+
+            if (product == null)
+            {
+                _logger.LogError($"Product with id: {id}, not found.");
+                return NotFound();
+            }
+
+            return Ok(product); 
+        }
+
+        [HttpGet]
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductByCategory(string category)
+        {
+            var products = await _productRepository.GetProductByCategory(category);
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.Created)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _productRepository.CreateProduct(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut(Name = "UpdateProduct")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            await _productRepository.UpdateProduct(product);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            await _productRepository.DeleteProduct(id);
+            return NoContent();
+        }
+
+    }
+}
