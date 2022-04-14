e68e450b99a01cd75a8e3a13c6a9ae18c58ea28f
Juste Zilinskaite <juste.zilinskaite@gmail.com>
2022-04-14 12:15:49 +0300

    Add Ordering Infrastructure project

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -1,17 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Ordering.Application;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ordering.Infrastructure;
 
 namespace Ordering.API
 {
@@ -29,7 +23,7 @@ namespace Ordering.API
         {
 
             services.AddApplicationServices();
-           // services.AddInfrastructureServices();
+            services.AddInfrastructureServices(Configuration);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
