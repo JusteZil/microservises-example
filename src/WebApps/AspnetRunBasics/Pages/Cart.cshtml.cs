e6e544b20e38bf0e7903752521488bf6bb9481af
Juste Zilinskaite <juste.zilinskaite@gmail.com>
2022-04-20 18:08:39 +0300

    Add web app microservice

---
+++ src/WebApps/AspnetRunBasics/Pages/Cart.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspnetRunBasics
+{
+    public class CartModel : PageModel
+    {
+        private readonly IBasketService _basketService;
+        public CartModel(IBasketService basketService)
+        {
+            _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+        }
+        public BasketModel Cart { get; set; } = new BasketModel();        
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var userName = "swn";
+            Cart = await _basketService.GetBasket(userName);            
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostRemoveToCartAsync(string productId)
+        {
+            var userName = "swn";
+            var basket = await _basketService.GetBasket(userName);
+
+            var item = basket?.Items.Single(x => x.ProductId == productId);
+            
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            basket.Items.Remove(item);
+
+            await _basketService.UpdateBasket(basket);
+            return RedirectToPage();
+        }
+    }
+}
